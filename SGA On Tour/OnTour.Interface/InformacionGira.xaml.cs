5f0d985597d333bfa79cfd4e59ce07d292fda286
Franckito <f.alvaradou@alumnos.duoc.cl>
2018-06-21 18:56:06 -0400

    Se crean algunas Interfaces falta la conexion y el negocio

--- SGA On Tour/OnTour.Interface/InformacionGira.xaml.cs
+++ SGA On Tour/OnTour.Interface/InformacionGira.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace OnTour.Interface
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para InformacionGira.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class InformacionGira : UserControl
     {
-        public MainWindow()
+        public InformacionGira()
         {
             InitializeComponent();
         }
---
+++ SGA On Tour/OnTour.Interface/LoginAlumno.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OnTour.Interface
+{
+    /// <summary>
+    /// Lógica de interacción para LoginAlumno.xaml
+    /// </summary>
+    public partial class LoginAlumno : Window
+    {
+        public LoginAlumno()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIngresar_Click(object sender, RoutedEventArgs e)
+        {
+            string user, clave;
+            user = TxtNombre.Text;
+            clave = TxtClave.Text;
+
+
+        }
+    }
+}
